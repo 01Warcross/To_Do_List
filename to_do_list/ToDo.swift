101a0563e23f0a80271a8358ccbd4dbfa532a43a
Claudia Pérez <iphone.claudia@icloud.com>
2019-06-24 11:53:37 -0500

    Iteration 0

---
+++ to_do_list/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  to_do_list
+//
+//  Created by Apple on 6/24/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+}
